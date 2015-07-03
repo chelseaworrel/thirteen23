674c47a743f73ed17de8a68023d3c06e6f1c6a13
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-03 14:59:13 -0600

    minitest gem added to Gemfile - minitest/autorun and minitest/pride added to test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require 'minitest/autorun'
+require 'minitest/pride'
 
 class ActiveSupport::TestCase
   fixtures :all
