5d0004475c0b23a0696799e60467ac32f66564dd
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-03 16:19:50 -0600

    can create a new user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+    else
+      flash.now[:errors] = "Invalid login credentials. Please try again"
+      render login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to users_path
+  end
 end
