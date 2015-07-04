62fc5c3ca10fa106ea65702f891eb7f51462f626
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-04 17:29:45 -0600

    users ordred by created_at date

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   has_attachment :avatar, accept: [:png]
+  User.order(created_at: :desc)
 
   def to_param
     "#{self.username}"
