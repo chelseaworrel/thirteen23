a528923cd8dde47817351e7cdbf5666371bea46f
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-04 17:15:57 -0600

    fixed tests with new wording - all tests passing

--- test/integration/user_can_sign_up_test.rb
+++ test/integration/user_can_sign_up_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UserCanSignUpTest < ActionDispatch::IntegrationTest
   def test_a_user_can_sign_up
     visit root_path
-    assert page.has_content?("Welcome")
     assert page.has_link?("Create Account")
     assert page.has_link?("Login")
     click_link_or_button 'Create Account'
@@ -14,11 +13,11 @@ class UserCanSignUpTest < ActionDispatch::IntegrationTest
                          email: "chelsea@gmail.com",
                          location: "Denver")
 
-    fill_in "Name", with: "Chelsea Worrel"
-    fill_in "Username", with: "cdub"
-    fill_in "Password", with: "password"
-    fill_in "Location", with: "Denver"
-    fill_in "Email", with: "chelsea@gmail.com"
+    fill_in "name", with: "Chelsea Worrel"
+    fill_in "username", with: "cdub"
+    fill_in "password", with: "password"
+    fill_in "location (optional)", with: "Denver"
+    fill_in "email", with: "chelsea@gmail.com"
     click_link_or_button 'Create Account'
 
     assert_equal username_path(user), current_path
@@ -27,10 +26,10 @@ class UserCanSignUpTest < ActionDispatch::IntegrationTest
   def test_it_throws_an_error_when_a_user_is_missing_name
     visit root_path
     click_link_or_button 'Create Account'
-    fill_in "Username", with: "cdub"
-    fill_in "Password", with: "password"
-    fill_in "Email", with: "chelsea@gmail.com"
-    fill_in "Name", with: ""
+    fill_in "username", with: "cdub"
+    fill_in "password", with: "password"
+    fill_in "email", with: "chelsea@gmail.com"
+    fill_in "name", with: ""
     click_button 'Create Account'
 
     assert page.has_content?("Name can't be blank")
@@ -39,10 +38,10 @@ class UserCanSignUpTest < ActionDispatch::IntegrationTest
   def test_it_throws_an_error_when_a_user_is_username
     visit root_path
     click_link_or_button 'Create Account'
-    fill_in "Username", with: ""
-    fill_in "Password", with: "password"
-    fill_in "Email", with: "chelsea@gmail.com"
-    fill_in "Name", with: "Chelsea Worrel"
+    fill_in "username", with: ""
+    fill_in "password", with: "password"
+    fill_in "email", with: "chelsea@gmail.com"
+    fill_in "name", with: "Chelsea Worrel"
     click_button 'Create Account'
 
     assert page.has_content?("Username can't be blank")
@@ -51,10 +50,10 @@ class UserCanSignUpTest < ActionDispatch::IntegrationTest
   def test_it_throws_an_error_when_a_user_is_username
     visit root_path
     click_link_or_button 'Create Account'
-    fill_in "Username", with: "cdub"
-    fill_in "Email", with: "chelsea@gmail.com"
-    fill_in "Password", with: ""
-    fill_in "Name", with: "Chelsea Worrel"
+    fill_in "username", with: "cdub"
+    fill_in "email", with: "chelsea@gmail.com"
+    fill_in "password", with: ""
+    fill_in "name", with: "Chelsea Worrel"
     click_button 'Create Account'
 
     assert page.has_content?("Password can't be blank")
@@ -68,11 +67,11 @@ class UserCanSignUpTest < ActionDispatch::IntegrationTest
                          location: "Denver")
     visit root_path
     click_link_or_button 'Create Account'
-    fill_in "Username", with: "cdub"
-    fill_in "Password", with: "password"
-    fill_in "Name", with: "Chelsea Worrel"
-    fill_in "Email", with: "chelsea@gmail.com"
-    fill_in "Location", with: ""
+    fill_in "username", with: "cdub"
+    fill_in "password", with: "password"
+    fill_in "name", with: "Chelsea Worrel"
+    fill_in "email", with: "chelsea@gmail.com"
+    fill_in "location (optional)", with: ""
     click_button 'Create Account'
 
     assert_equal username_path(user), current_path
