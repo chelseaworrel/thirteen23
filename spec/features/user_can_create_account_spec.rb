a0cc5600c48f33804074479fba707d098bce2021
Chelsea Worrel <chelseaworrel@gmail.com>
2015-07-05 09:33:56 -0600

    added more create account tests - passing

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -14,7 +14,7 @@ describe "user creates account", type: :feature do
     expect(page).to have_content("Chelsea")
   end
 
-  it "user can't create account without all required fields" do
+  it "user can't create account without username" do
     visit root_path
     click_link("Create Account")
     page.fill_in "password", with: "password"
@@ -22,6 +22,28 @@ describe "user creates account", type: :feature do
     click_button("Create Account")
 
     expect(current_path).to eq(new_user_path)
-    expect(page).to_not have_content("Chelsea")
+    expect(page).to have_content("Username can't be blank")
+  end
+
+  it "user can't create account without name" do
+    visit root_path
+    click_link("Create Account")
+    page.fill_in "password", with: "password"
+    page.fill_in "username", with: "cdub"
+    click_button("Create Account")
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it "user can't create account without password" do
+    visit root_path
+    click_link("Create Account")
+    page.fill_in "name", with: "Chelsea Worrel"
+    page.fill_in "username", with: "cdub"
+    click_button("Create Account")
+
+    expect(current_path).to eq(new_user_path)
+    expect(page).to have_content("Password can't be blank")
   end
 end
